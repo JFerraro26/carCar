217513f29f17c424a8f834b55298862c1e7ca178
JoeFerraro <jferraro262@gmail.com>
2023-04-24 22:48:27 -0700

    sales poller working

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import AutomobileVO, Salesperson, Customer, Sale
+from common.json import ModelEncoder
+
 
 # Create your views here.
+class AutomobileVoEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "vin",
+        "color",
+        "year",
+        "import_href",
+    ]
+
+class SalespersonEncoder(ModelEncoder):
+    model = Salesperson
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "first_name",
+        "last_name",
+        "address",
+        "phone_number",
+    ]
+
+class SaleEncoder(ModelEncoder):
+    model = Sale
+    properties = [
+        "automobile",
+        "salesperson",
+        "customer",
+        "price",
+    ]
+    encoders = {
+        "automobile" : AutomobileVoEncoder(),
+        "salesperson" : SalespersonEncoder(),
+        "customer" : CustomerEncoder(),
+    }

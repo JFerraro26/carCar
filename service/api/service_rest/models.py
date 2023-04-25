49181b494fe2837d4ec314e662245e856068fab7
Miguel Robles <migarob@icloud.com>
2023-04-24 19:00:38 -0700

    commit before pulling front-end automobile

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.IntegerField()
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    vin = models.ForeignKey(AutomobileVO, on_delete=models.CASCADE)
+    customer = models.CharField(max_length=100)
+    technician = models.ForeignKey(Technician, on_delete=models.CASCADE)
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -9,18 +9,28 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
 
-# Import models from service_rest, here.
-# from service_rest.models import Something
+from service_rest.models import AutomobileVO
+
+def get_vins():
+    response = requests.get('http://localhost:8000/api/automobiles/')
+    if response.ok:
+        data = response.json()
+        return [automobile['vin'] for automobile in data]
+    else:
+        raise ValueError('Error retrieving VINs:', response.status_code)
 
 def poll():
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
-            pass
+            vins = get_vins()
+
+            for vin in vins:
+                AutomobileVO.objects.update_or_create(vin=vin)
         except Exception as e:
-            print(e, file=sys.stderr)
+            print('Error polling for VINs:', e, file=sys.stderr)
         time.sleep(60)
+
 
 
 if __name__ == "__main__":
